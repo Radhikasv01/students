e21236c6c824c75c5d18731390811467ea7395fb
Radhika <rathikaselvaraj01@gmail.com>
2025-08-02 12:46:01 +0530

    six

--- my-app/src/Components/Login.js
+++ my-app/src/Components/Login.js
@@ -1,6 +1,8 @@
-// src/Components/Login.js
 import React, { useState } from 'react';
-import { Button, TextField, Container, Typography, Box, Snackbar, Alert } from '@mui/material';
+import {
+  Button, TextField, Container, Typography, Box, Snackbar, Alert, IconButton, InputAdornment
+} from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../axios';
 
@@ -10,6 +12,7 @@ const Login = () => {
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState('');
   const [snackbarSeverity, setSnackbarSeverity] = useState('info');
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -24,6 +27,7 @@ const Login = () => {
     const validationErrors = {};
     if (!username) validationErrors.username = 'Username is required';
     if (!password) validationErrors.password = 'Password is required';
+
     if (Object.keys(validationErrors).length > 0) {
       setErrors(validationErrors);
       return;
@@ -65,14 +69,19 @@ const Login = () => {
     navigate('/');
   };
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <Container maxWidth="xs" sx={{ mt: 8 }}>
-      <Typography variant="h4" component="h1" gutterBottom>
+      <Typography variant="h4" component="h1" gutterBottom align="center">
         Login
       </Typography>
       <Box component="form" onSubmit={handleLogin} noValidate>
         <TextField
           fullWidth
+          autoFocus
           label="Username"
           name="username"
           margin="normal"
@@ -85,24 +94,55 @@ const Login = () => {
           fullWidth
           label="Password"
           name="password"
-          type="password"
+          type={showPassword ? 'text' : 'password'}
           margin="normal"
           value={form.password}
           onChange={handleChange}
           error={!!errors.password}
           helperText={errors.password}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={togglePasswordVisibility} edge="end">
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
         />
         <Box display="flex" justifyContent="space-between" mt={2}>
-          <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mr: 1 }}>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            sx={{ mr: 1 }}
+          >
             Login
           </Button>
-          <Button onClick={handleCancel} variant="outlined" color="secondary" fullWidth sx={{ ml: 1 }}>
+          <Button
+            onClick={handleCancel}
+            variant="outlined"
+            color="secondary"
+            fullWidth
+            sx={{ ml: 1 }}
+          >
             Cancel
           </Button>
         </Box>
       </Box>
-      <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={() => setSnackbarOpen(false)}>
-        <Alert severity={snackbarSeverity} onClose={() => setSnackbarOpen(false)}>
+
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={4000}
+        onClose={() => setSnackbarOpen(false)}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      >
+        <Alert
+          severity={snackbarSeverity}
+          onClose={() => setSnackbarOpen(false)}
+          sx={{ width: '100%' }}
+        >
           {snackbarMessage}
         </Alert>
       </Snackbar>
